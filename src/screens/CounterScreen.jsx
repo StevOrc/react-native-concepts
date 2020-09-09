49f94d2a76f53eeda23715fe9c0ffad736e97c46
StevOrc <steven.orcun2@gmail.com>
2020-09-09 14:34:42 +0200

    some test on counter

--- src/screens/CounterScreen.jsx
+++ src/screens/CounterScreen.jsx
@@ -3,11 +3,36 @@ import { View, Text, StyleSheet, Button } from "react-native";
 
 const CounterScreen = () => {
   const [counter, setCounter] = useState(0);
+  const [isNegative, setIsNegative] = useState(false);
+
+  const handleOnDecrease = () => {
+    if (counter - 1 < 0) {
+      setCounter(counter);
+      setIsNegative(true);
+    } else {
+      setCounter(counter - 1);
+      setIsNegative(false);
+    }
+  };
+
+  const handleOnIncrease = () => {
+    setCounter(counter + 1);
+    setIsNegative(false);
+  };
+
+  const renderErrorMessage = () => {
+    if (isNegative)
+      return (
+        <Text style={{ color: "red" }}> Counter can not be negative </Text>
+      );
+  };
+
   return (
     <View>
-      <Button title="Icrease" onPress={() => setCounter(counter + 1)}></Button>
-      <Button title="Decrease" onPress={() => setCounter(counter - 1)}></Button>
+      <Button title="Icrease" onPress={() => handleOnIncrease()}></Button>
+      <Button title="Decrease" onPress={() => handleOnDecrease()}></Button>
       <Text>Counter Count {counter}</Text>
+      {renderErrorMessage()}
     </View>
   );
 };
